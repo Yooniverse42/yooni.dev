d19d2e26b4b05935112bb2df831be17c28badc51
Yooniverse42 <162732401+Yooniverse42@users.noreply.github.com>
2025-05-23 00:07:07 +0900

    개발용, 배포용 클래스명 분리

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,21 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
+const isDev = process.env.NODE_ENV === "development";
+
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
+    },
+  },
+  css: {
+    devSourcemap: true,
+    modules: {
+      generateScopedName: isDev
+        ? "[name]_[local]_[hash:base64:4]"
+        : "[local]_[hash:base64:2]",
     },
   },
 });
