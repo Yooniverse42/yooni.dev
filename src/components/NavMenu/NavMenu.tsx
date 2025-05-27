6f6acf7238beb0f269176352d1069f89022c5ec0
Yooniverse42 <162732401+Yooniverse42@users.noreply.github.com>
2025-05-27 15:30:18 +0900

    내비게이션 메뉴 컴포넌트 생성

---
+++ src/components/NavMenu/NavMenu.tsx
@@ -0,0 +1,37 @@
+import { NavLink } from 'react-router-dom';
+import styles from '@/components/NavMenu/NavMenu.module.css';
+
+const navList = [
+  ['/', 'ABOUT'],
+  ['/project', 'PROJECT'],
+  ['/resume', 'RESUME'],
+  ['https://velog.io/@yxxnicode/posts', 'BLOG'],
+];
+
+export default function NavMenu() {
+  return (
+    <nav className={styles.nav}>
+      <ul role="menubar" className={styles.wrapper}>
+        {navList.map(([to, title]) => (
+          <li key={to} role="none" className={styles.list}>
+            {to.startsWith('http') ? (
+              <a
+                href={to}
+                target="_blank"
+                rel="noopener noreferer"
+                role="menuitem"
+                className={styles.link}
+              >
+                {title}
+              </a>
+            ) : (
+              <NavLink to={to} role="menuitem" className={styles.link}>
+                {title}
+              </NavLink>
+            )}
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+}
