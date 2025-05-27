2287c4d04e036355e67826e9cb7571f8fef863e9
Yooniverse42 <162732401+Yooniverse42@users.noreply.github.com>
2025-05-27 23:28:40 +0900

    페이지 생성

---
+++ src/pages/About.tsx
@@ -0,0 +1,18 @@
+export default function About() {
+  return (
+    <>
+      <main className="main">
+        <p
+          style={{
+            color: 'var(--color-primary)',
+            fontWeight: '700',
+          }}
+        >
+          JIYUN | FRONTEND DEVELOPER
+          <br />
+          안녕하세요, 프론트엔드 개발자 김지윤 입니다.
+        </p>
+      </main>
+    </>
+  );
+}
---
+++ src/pages/index.ts
@@ -0,0 +1,4 @@
+export { default as About } from '@/pages/About';
+export { default as Project } from '@/pages/Project';
+export { default as Resume } from '@/pages/Resume';
+export { default as NotFound } from '@/pages/NotFound';
