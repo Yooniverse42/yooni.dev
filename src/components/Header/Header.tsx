21d416efe368cb85e37a01110d39f91ff41bbecc
Yooniverse42 <162732401+Yooniverse42@users.noreply.github.com>
2025-05-29 13:19:46 +0900

    상단에 헤더 고정 및 스타일 수정

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,23 +6,24 @@ import { Link } from 'react-router-dom';
 export default function Header() {
   return (
     <header role="banner" className={styles.container}>
-      <div className={styles.wrapper}>
-        <h1 className={styles.head}>
-          <span className="sr-only">
-            프론트엔드 개발자 김지윤의 포트폴리오 입니다.
-          </span>
-          <Link to={'/'} className={styles.moveHome}>
-            <img
-              src="/favicons/android-chrome-512x512.png"
-              alt="about으로 이동"
-              className={styles.moveHomeImg}
-            />
-          </Link>
-        </h1>
-        <NavMenu />
+      <div className={styles.headerWrapper}>
+        <div className={styles.titleNavWrapper}>
+          <h1 className={styles.logoTitle}>
+            <span className="sr-only">
+              프론트엔드 개발자 김지윤의 포트폴리오 입니다.
+            </span>
+            <Link to={'/'} className={styles.homeLink}>
+              <img
+                src="/favicons/android-chrome-512x512.png"
+                alt="about으로 이동"
+                className={styles.homeLogoImg}
+              />
+            </Link>
+          </h1>
+          <NavMenu />
+        </div>
+        <ThemeToggle />
       </div>
-
-      <ThemeToggle />
     </header>
   );
 }
