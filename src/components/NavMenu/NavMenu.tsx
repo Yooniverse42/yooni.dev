2c1c2da686107630d91bf8b9a1f61dd6940b6e7c
Yooniverse42 <162732401+Yooniverse42@users.noreply.github.com>
2025-06-18 22:57:37 +0900

    반응형 내비게이션 메뉴로 변경

--- src/components/NavMenu/NavMenu.tsx
+++ src/components/NavMenu/NavMenu.tsx
@@ -1,34 +1,44 @@
 import { NavLink } from 'react-router-dom';
 import styles from '@/components/NavMenu/NavMenu.module.css';
+import clsx from 'clsx';
 
 const navList = [
   ['/', 'ABOUT'],
   ['/project', 'PROJECT'],
   ['/resume', 'RESUME'],
-  ['https://velog.io/@yxxnicode/posts', 'BLOG'],
 ];
 
-export default function NavMenu() {
+export default function NavMenu({ onClose }: { onClose?: () => void }) {
   return (
     <nav className={styles.container}>
-      <ul role="menubar" className={styles.wrapper}>
+      <ul role="menubar" className={clsx(styles.wrapperDesk, 'desktopOnly')}>
         {navList.map(([to, title]) => (
-          <li key={to} role="none" className={styles.list}>
-            {to.startsWith('http') ? (
-              <a
-                href={to}
-                target="_blank"
-                rel="noopener noreferer"
-                role="menuitem"
-                className={styles.link}
-              >
-                {title}
-              </a>
-            ) : (
-              <NavLink to={to} role="menuitem" className={styles.link}>
-                {title}
-              </NavLink>
-            )}
+          <li key={to} role="none" className={clsx(styles.list, 'flexCenter')}>
+            <NavLink
+              to={to}
+              role="menuitem"
+              className={({ isActive }) =>
+                clsx(styles.link, isActive && styles.active)
+              }
+            >
+              {title}
+            </NavLink>
+          </li>
+        ))}
+      </ul>
+      <ul role="menubar" className={clsx(styles.wrapperMobile, 'mobileOnly')}>
+        {navList.map(([to, title]) => (
+          <li key={to} role="none" className={clsx(styles.list, 'flexCenter')}>
+            <NavLink
+              to={to}
+              role="menuitem"
+              className={({ isActive }) =>
+                clsx(styles.link, isActive && styles.active)
+              }
+              onClick={onClose}
+            >
+              {title}
+            </NavLink>
           </li>
         ))}
       </ul>
