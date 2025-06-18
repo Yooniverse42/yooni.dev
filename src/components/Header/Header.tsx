d4b0373d5cb53f6eeab8eba6e1c3b42ca571c8a0
Yooniverse42 <162732401+Yooniverse42@users.noreply.github.com>
2025-06-18 23:07:27 +0900

    반응형 헤더로 변경

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,28 +1,60 @@
-import ThemeToggle from '@/components/ThemeToggle/ThemeToggle';
+import ThemeToggle from '@/components/Button/ThemeToggle';
 import styles from '@/components/Header/Header.module.css';
 import NavMenu from '@/components/NavMenu/NavMenu';
 import { Link } from 'react-router-dom';
+import MenuToggle from '@/components/Button/MenuToggle';
+import clsx from 'clsx';
+import { useState } from 'react';
 
 export default function Header() {
+  const [isOpen, setOpen] = useState(false);
+
+  const handleMode = (e: React.KeyboardEvent<HTMLOrSVGElement>) => {
+    if (e.key === 'Enter' || e.key === ' ') {
+      setOpen((prev) => !prev);
+      e.preventDefault();
+    }
+  };
+
   return (
     <header role="banner" className={styles.container}>
       <div className={styles.headerWrapper}>
-        <div className={styles.titleNavWrapper}>
-          <h1 className={styles.logoTitle}>
-            <span className="sr-only">
-              프론트엔드 개발자 김지윤의 포트폴리오 입니다.
-            </span>
-            <Link to={'/'} className={styles.homeLink}>
-              <img
-                src="/favicons/android-chrome-512x512.png"
-                alt="about으로 이동"
-                className={styles.homeLogoImg}
-              />
-            </Link>
-          </h1>
+        <h1 className={styles.logoTitle}>
+          <Link to={'/'} className={styles.homeLink}>
+            <img
+              src="/favicons/android-chrome-512x512.png"
+              alt="about으로 이동"
+              className={styles.homeLogoImg}
+            />
+          </Link>
+          <p className={styles.title}>김지윤 | 프론트엔드 개발자</p>
+        </h1>
+        <div className={clsx(styles.navMenuWrapperDesk, 'desktopOnly')}>
           <NavMenu />
         </div>
-        <ThemeToggle />
+        <div className={clsx(styles.themeToggleWrapperDesk, 'desktopOnly')}>
+          <ThemeToggle />
+        </div>
+        <div
+          className={clsx(isOpen && styles.floatMenu, 'mobileOnly')}
+          tabIndex={0}
+          onKeyDown={handleMode}
+        >
+          <MenuToggle isOpen={isOpen} setOpen={setOpen} />
+        </div>
+        {isOpen && (
+          <>
+            <div className={styles.dimmed} onClick={() => setOpen(false)}></div>
+            <div className={clsx(styles.openMenu, 'mobileOnly')}>
+              <div className={clsx(styles.navMenuWrapper, 'mobileOnly')}>
+                <NavMenu onClose={() => setOpen(false)} />
+              </div>
+              <div className={clsx(styles.themeToggleWrapper, 'mobileOnly')}>
+                <ThemeToggle />
+              </div>
+            </div>
+          </>
+        )}
       </div>
     </header>
   );
