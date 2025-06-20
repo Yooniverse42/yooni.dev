8cb8ead160df7d57ee639881c84867bbf21b17e7
Yooniverse42 <162732401+Yooniverse42@users.noreply.github.com>
2025-06-20 14:54:54 +0900

    클래스명 변경

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -5,14 +5,14 @@ import clsx from 'clsx';
 export default function About() {
   return (
     <>
-      <section className={styles.section01}>
+      <section className={styles.profileSection}>
         <h2>Glad you’re here!</h2>
         <img
           src="images/profile-about.jpeg"
           alt="프로필 사진"
           className={styles.profile}
         />
-        <div className={clsx(styles.section01Desc, 'flexCenter')}>
+        <div className={clsx(styles.introduction, 'flexCenter')}>
           <p>안녕하세요, 클릭해 주셔서 감사합니다.</p>
           <div className="flexCenter">
             <p>프론트엔드 개발자 김지윤 입니다.</p>
@@ -21,7 +21,7 @@ export default function About() {
         </div>
       </section>
       <div className={styles.sectionLine}></div>
-      <section className={styles.section02}>
+      <section className={styles.valuesSection}>
         <div className={styles.cardWrapper}>
           <AboutCard icon="ear" />
           <AboutCard icon="book" changeIcon={true} />
