ef2a27aa07e458cf3e0e371d697f33e520a27fe3
Yooniverse42 <162732401+Yooniverse42@users.noreply.github.com>
2025-06-20 14:51:32 +0900

    이미지 경로 및 스타일 수정

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -7,12 +7,14 @@ export default function NotFound() {
     <div className={clsx(styles.container, 'flexCenter')}>
       <section className={clsx(styles.wrapper, 'flexCenter')}>
         <div className={clsx(styles.invalidPath, 'flexCenter')}>
-          <img src="/icons/ghost.png" alt="ghost" aria-hidden={true} />
-          <p>404 NOT FOUND</p>
-          <img src="/icons/ghost.png" alt="ghost" aria-hidden={true} />
+          <img src="/images/ghost.png" alt="ghost" aria-hidden={true} />
+          <h2>404 NOT FOUND</h2>
+          <img src="/images/ghost.png" alt="ghost" aria-hidden={true} />
         </div>
-        <p>존재하지 않는 경로입니다.</p>
-        <p>하지만 프론트엔드 개발자 김지윤은 존재합니다!</p>
+        <div className={styles.desc}>
+          <p>존재하지 않는 경로입니다.</p>
+          <p>하지만 프론트엔드 개발자 김지윤은 존재합니다!</p>
+        </div>
         <Link to={'/'} className={styles.toAbout}>
           저에 대해서 알아보시겠어요?
         </Link>
