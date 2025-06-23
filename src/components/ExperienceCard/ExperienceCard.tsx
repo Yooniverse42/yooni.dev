9332a4d455f9fdd603f7add98226cdc798b68c68
Yooniverse42 <162732401+Yooniverse42@users.noreply.github.com>
2025-06-23 23:25:51 +0900

    Resume 페이지에 들어갈 경험 및 교육 카드 컴포넌트

---
+++ src/components/ExperienceCard/ExperienceCard.tsx
@@ -0,0 +1,56 @@
+import styles from '@/components/ExperienceCard/ExperienceCard.module.css';
+import clsx from 'clsx';
+
+interface ExperienceCardProps {
+  title: string;
+  subTitle: string;
+  date: string;
+  desc?: string[];
+  edu?: boolean;
+}
+
+export default function ExperienceCard({
+  title,
+  subTitle,
+  date,
+  desc,
+  edu = false,
+}: ExperienceCardProps) {
+  const period = (date: string) => {
+    const split = date.split(' ');
+    const start = split[0];
+    const end = split[2];
+    const timeStart = start.replace('.', '-');
+    const endStart = end.replace('.', '-');
+    return (
+      <>
+        <time dateTime={timeStart}>{start}</time>
+        <span> {split[1]} </span>
+        <time dateTime={endStart}>{end}</time>
+      </>
+    );
+  };
+
+  return edu ? (
+    <li className={styles.eduContainer}>
+      <header className={styles.eduHeader}>
+        <h5 className={styles.eduWrapper}>
+          <p className={clsx(styles.title, styles.eduTitle)}>{title}</p>
+          <p className={styles.subTitle}>{subTitle}</p>
+        </h5>
+        <p className={styles.period}>{period(date)}</p>
+      </header>
+    </li>
+  ) : (
+    <li className={styles.container}>
+      <header className={clsx(styles.header, styles.cardHeader)}>
+        <h5 className={styles.headingWrapper}>
+          <p className={styles.title}>{title}</p>
+          <p className={styles.subTitle}>{subTitle}</p>
+        </h5>
+        <p className={styles.period}>{period(date)}</p>
+      </header>
+      <p className={styles.desc}>{desc}</p>
+    </li>
+  );
+}
