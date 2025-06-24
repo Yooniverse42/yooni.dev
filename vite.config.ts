a636d87b5db99f96e7e1d9c6dbb703ceeaf2eb58
Yooniverse42 <162732401+Yooniverse42@users.noreply.github.com>
2025-06-24 15:43:57 +0900

    supabase 설치

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,23 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-const isDev = process.env.NODE_ENV === "development";
+const isDev = process.env.NODE_ENV === 'development';
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
+      '@': path.resolve(__dirname, 'src'),
     },
   },
   css: {
     devSourcemap: true,
     modules: {
       generateScopedName: isDev
-        ? "[name]_[local]_[hash:base64:4]"
-        : "[local]_[hash:base64:2]",
+        ? '[name]_[local]_[hash:base64:4]'
+        : '[local]_[hash:base64:2]',
     },
   },
+  envPrefix: 'VITE_',
 });
